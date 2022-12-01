9c313caca1dd85a6b0198ed8f2dcb8f3bdf3b417
Jonathan Carroll <github@jcarroll.com.au>
2022-12-01 22:43:25 +1030

    day01

---
+++ inst/run-day01.R
@@ -0,0 +1,10 @@
+library(adventofcode22)
+x <- readLines("./inst/input01.txt")
+# x <- gsub("^$", "\n", x)
+x <- paste(x, collapse = "\n")
+
+p1 <- f01a(x)
+p2 <- f01b(x)
+
+stopifnot(p1 == aoc_solutions$day01a)
+stopifnot(p2 == aoc_solutions$day01b)
