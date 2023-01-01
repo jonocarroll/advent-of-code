3cbdb2158ab3b7b2472bd460f8d21bd82e731b8c
Jonathan Carroll <github@jcarroll.com.au>
2023-01-01 11:50:59 +1030

    day05

--- 2022/Rust/src/bin/05.rs
+++ 2022/Rust/src/bin/05.rs
@@ -1,67 +1,104 @@
 use regex::Regex;
 
 pub fn part_one(input: &str) -> Option<String> {
-    // let parts = 
+    // let parts =
     let (stacks, instr) = parse05(&input);
-    
-    println!("{:?}", stacks.crates());
-    println!("{:?}", instr.parse());
-    None
+    let mut crates = stacks.crates();
+
+    for i in 0..instr.len() {
+        // println!("{:?}", instr[i].parse());
+        // println!("{:?}", crates);
+        crates = crane(crates, instr[i].parse());
+        // println!("{:?}", crates);
+    }
+    let tops = crates.iter().map(|s| s.last().unwrap()).collect::<String>();
+    Some(tops)
 }
 
-pub fn part_two(_input: &str) -> Option<String> {
-    None
+pub fn part_two(input: &str) -> Option<String> {
+    // let parts =
+    let (stacks, instr) = parse05(&input);
+    let mut crates = stacks.crates();
+
+    for i in 0..instr.len() {
+        // println!("{:?}", instr[i].parse());
+        // println!("{:?}", crates);
+        crates = crane9001(crates, instr[i].parse());
+        // println!("{:?}", crates);
+    }
+    let tops = crates.iter().map(|s| s.last().unwrap()).collect::<String>();
+    Some(tops)
+}
+
+fn crane(crates: Vec<Vec<char>>, instr: (u32, u32, u32)) -> Vec<Vec<char>> {
+    let n: u32 = instr.0;
+    // println!("n = {:?}", n);
+    let mut tmpcrates = crates.clone(); //TODO use references!
+    for _i in 0..n {
+        let tomove: char = tmpcrates[(instr.1 - 1) as usize].pop().unwrap();
+        tmpcrates[(instr.2 - 1) as usize].push(tomove);
+    }
+    tmpcrates
+}
+
+fn crane9001(crates: Vec<Vec<char>>, instr: (u32, u32, u32)) -> Vec<Vec<char>> {
+    let n = instr.0 as usize;
+    let mut tmpcrates = crates.clone(); 
+    let new_len = tmpcrates[(instr.1 - 1) as usize].len();
+    let mut tomove = vec![];
+    for _i in 0..n {
+        tomove.push(tmpcrates[(instr.1 - 1) as usize].pop().unwrap());
+    }
+    tomove.reverse();
+    tmpcrates[(instr.1 - 1) as usize].truncate(new_len - n);
+    for x in tomove.into_iter() {
+        tmpcrates[(instr.2 - 1) as usize].push(x);
+    }
+    tmpcrates
 }
 
 #[derive(Debug)]
 struct Stacks {
-    stacks: String
+    stacks: String,
 }
 
 impl Stacks {
     fn crates(&self) -> Vec<Vec<char>> {
-        let stacklines = &self.stacks
-        .lines()
-        .into_iter()
-        .map(|x| x.chars().collect::<Vec<char>>())
-        .collect::<Vec<Vec<char>>>();
-    let mut stackentries = vec![];
-    for l in stacklines.iter() {
-        stackentries.push(l.iter().skip(1).step_by(4).collect::<Vec<&char>>());
-    }
-    // reshape to stacks
-    let mut stack = vec![vec![' '; stackentries.len()-1]; stackentries[1].len()];
-    for s in 0..stackentries.len()-1 {
-        // let items = ;
-        for el in 0..stackentries[s].len() {
-            stack[el][s] = stackentries[s][el].to_owned()
+        let stacklines = &self
+            .stacks
+            .lines()
+            .into_iter()
+            .map(|x| x.chars().collect::<Vec<char>>())
+            .collect::<Vec<Vec<char>>>();
+        let mut stackentries = vec![];
+        for l in stacklines.iter() {
+            stackentries.push(l.iter().skip(1).step_by(4).collect::<Vec<&char>>());
         }
-    }
-    for s in 0..stack.len() {
-        stack[s].reverse();
-        stack[s].retain(|x| *x != ' ');
-    }
+        // reshape to stacks
+        let mut stack = vec![vec![' '; 50]; stackentries[1].len()];
+        for s in 0..stackentries.len() - 1 {
+            // let items = ;
+            for el in 0..stackentries[s].len() {
+                stack[el][s] = stackentries[s][el].to_owned()
+            }
+        }
+        for s in 0..stack.len() {
+            stack[s].reverse();
+            stack[s].retain(|x| *x != ' ');
+        }
 
-    stack
+        stack
     }
 }
 
-
 #[derive(Debug)]
 struct Instructions {
-    input: String
+    input: String,
 }
 
 impl Instructions {
     fn parse(&self) -> (u32, u32, u32) {
-        // if let [Ok(tomove), Ok(from), Ok(to)] = &self.input.split(" ")
-        // .map(|a| a.parse::<u32>())
-        // .collect::<Vec<_>>()[..] {
-        //     (tomove.to_owned(), from.to_owned(), to.to_owned())
-        // } else {
-        //     (0, 0, 0)
-        // }
-        let re = Regex::new(r"move (\d) from (\d) to (\d)").unwrap();
+        let re = Regex::new(r"move (\d*) from (\d*) to (\d*)").unwrap();
         let caps = re.captures(&self.input).unwrap();
         let moveto = caps[1].parse::<u32>().unwrap();
         let from = caps[2].parse::<u32>().unwrap();
@@ -71,11 +108,18 @@ impl Instructions {
     }
 }
 
-fn parse05(input: &str) -> (Stacks, Instructions) {
+fn parse05(input: &str) -> (Stacks, Vec<Instructions>) {
     let parts = input.split_once("\n\n").unwrap();
-    let stacks = Stacks { stacks: String::from(parts.0) };
-    println!("{:?}", parts.1);
-    let instr = Instructions { input: String::from(parts.1) };
+    let stacks = Stacks {
+        stacks: String::from(parts.0),
+    };
+    let instr = parts
+        .1
+        .lines()
+        .map(|x| Instructions {
+            input: String::from(x),
+        })
+        .collect::<Vec<_>>();
     (stacks, instr)
 }
 
@@ -93,12 +137,12 @@ mod tests {
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 5);
         // println!("{:?}", parse05(input));
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(String::from("CMZ")));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 5);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(String::from("MCD")));
     }
 }
