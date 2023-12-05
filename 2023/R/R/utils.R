420add7222046dc628671e116f2e0926e15d3c2c
Jonathan Carroll <github@jcarroll.com.au>
2023-12-05 22:01:24 +1030

    ex and input helpers

--- 2023/R/R/utils.R
+++ 2023/R/R/utils.R
@@ -1,4 +1,18 @@
 #' @export
 ex <- function(n) {
-  readLines(paste0("inst/example", n,".txt"))
+  # for i in {1..25}; do touch "inst/example${(l(2)(0))i}.txt"; done
+  # for i in {1..25}; do touch "inst/example${(l(2)(0))i}b.txt"; done
+  day <- if (grepl("b", n, fixed = TRUE)) {
+    n <- sub("b", "", n)
+    paste0(sprintf("%02d", as.integer(n)), "b")
+  } else {
+    sprintf("%02d", as.integer(n))
+  }
+  readLines(paste0("inst/example", day,".txt"))
+}
+
+#' @export
+input <- function(n) {
+  day <- sprintf("%02d", as.integer(n))
+  readLines(paste0("inst/input", day,".txt"))
 }
