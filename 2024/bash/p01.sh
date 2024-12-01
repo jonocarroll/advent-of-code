63b4db1c4248c6448d498554aeb05d2102113394
Jonathan Carroll <github@jcarroll.com.au>
2024-12-01 21:41:39 +1030

    day 1 zsh part 2

--- 2024/bash/p01.sh
+++ 2024/bash/p01.sh
@@ -1,8 +1,20 @@
 #!/bin/zsh
 
 infile='../R/inst/input01.txt'
+
+## Part 1
 x=$(awk '{print $1}' $infile | sort)
 y=$(awk '{print $2}' $infile | sort)
 diffs=$(paste -d- <(echo $x) <(echo $y) | bc)
 paste -d+ -s <(echo $diffs | sed 's/-//g') | bc
+
+## Part 2
+xs=$(echo $x | uniq)
+acc=0
+for i in $(echo $xs); do
+    ((acc += $i * $(grep -c $i <(echo $y))))
+done
+echo $acc
+
+
 
