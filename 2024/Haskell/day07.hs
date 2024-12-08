cffde8f12c595d1170af5427e5210fbfc2adcc19
Jonathan Carroll <github@jcarroll.com.au>
2024-12-08 14:24:30 +1030

    day 7 both Haskell

---
+++ 2024/Haskell/day07.hs
@@ -0,0 +1,32 @@
+import Control.Arrow ((&&&))
+
+parse :: String -> [(Integer, [Integer])]
+parse = map (parseLine . words) . lines
+
+parseLine :: (Read a, Read b) => [[Char]] -> (a, [b])
+parseLine (x:xs) = (read (init x), map read xs)
+
+apply_funs :: [Integer] -> Integer -> [Integer]
+apply_funs x y = concatMap (\val -> [val + y, val * y]) x
+
+concat' :: (Num a, Show a) => a -> a -> a
+concat' a b = a * (10 ^ (length (show b))) + b
+
+apply_funs_2 :: [Integer] -> Integer -> [Integer]
+apply_funs_2 x y = concatMap (\val -> [val + y, val * y, concat' val y]) x
+
+test_ops :: Integer -> [Integer] -> [Integer]
+test_ops z (x:xs) = foldl (\acc x -> filter (<=z) $ apply_funs acc x) [x] xs
+
+test_ops_2 :: Integer -> [Integer] -> [Integer]
+test_ops_2 z (x:xs) = foldl (\acc x -> filter (<=z) $ apply_funs_2 acc x) [x] xs
+
+main :: IO ()
+main = do
+    p <- readFile "../R/inst/input07.txt"
+    --p <- readFile "../tmp.txt"
+    let parsed = parse p
+    let (res, vals) = (map fst &&& map snd) parsed
+    print $ sum $ zipWith (\x y -> if any (==x) (test_ops x y) then x else 0) res vals
+    print $ sum $ zipWith (\x y -> if any (==x) (test_ops_2 x y) then x else 0) res vals
+
