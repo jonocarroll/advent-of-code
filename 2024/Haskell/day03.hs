771ab4a2c6ba0f678d6de108e059c489ef22e946
Jonathan Carroll <github@jcarroll.com.au>
2024-12-23 17:11:55 +1030

    use an enum

--- 2024/Haskell/day03.hs
+++ 2024/Haskell/day03.hs
@@ -23,13 +23,15 @@ recurseMatches2 x = case getDoDontMatches x of
 getDigits :: String -> Int
 getDigits = doMult . map (filter isDigit) . splitOn "," 
 
-keepMuls :: Int -> [String] -> [[Char]] -> [String]
+data Active = On | Off deriving Eq
+
+keepMuls :: Active -> [String] -> [[Char]] -> [String]
 keepMuls _ s [] = s
-keepMuls isActive s (x:xs) 
-    | take 5 x == "don't" = keepMuls 0 s xs 
-    | take 2 x == "do" = keepMuls 1 s xs 
-    | isActive == 1 && take 3 x == "mul" = keepMuls 1 (s ++ [x]) xs 
-    | isActive == 0 && take 3 x == "mul" = keepMuls 0 s xs 
+keepMuls onoff s (x:xs) 
+    | take 7 x == "don't()" = keepMuls Off s xs 
+    | take 4 x == "do()" = keepMuls On s xs 
+    | onoff == On && take 3 x == "mul" = keepMuls On (s ++ [x]) xs 
+    | onoff == Off && take 3 x == "mul" = keepMuls Off s xs 
     | otherwise = error "Something else"
 
 doMult :: (Num a, Read a) => [String] -> a
@@ -41,4 +43,4 @@ main = do
     -- p <- readFile "tmp.txt"
     p <- readFile "R/inst/input03.txt"
     print $ sum $ recurseMatches p
-    print $ sum $ map getDigits $ keepMuls 1 [] (recurseMatches2 p)
+    print $ sum $ map getDigits $ keepMuls On [] (recurseMatches2 p)
