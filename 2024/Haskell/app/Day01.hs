4e08893cd3916c902d94aa5bf32370d63a7021b2
Jonathan Carroll <github@jcarroll.com.au>
2025-01-01 13:03:51 +1030

    day 01 Haskell perf

--- 2024/Haskell/app/Day01.hs
+++ 2024/Haskell/app/Day01.hs
@@ -2,8 +2,11 @@ module Day01 where
 
 import Data.List
 
-parse :: String -> [[Int]]
-parse = map (map read . words) . lines
+parse :: String -> [(Int, Int)]
+parse = map (toTuple . map read . words) . lines
+  where
+    toTuple [x, y] = (x, y)
+    toTuple _      = error "Invalid input format"
 
 score :: [Int] -> Int -> Int
 score y x = x * (length $ filter (==x) y)
@@ -12,8 +15,9 @@ day01 :: IO ()
 day01 = do
     p <- readFile "../R/inst/input01.txt"
     let valpairs = parse p
-    let x = map (!! 0) valpairs
-    let y = map (!! 1) valpairs
-    print $ sum $ map abs $ zipWith (-) (sort y) (sort x) -- part 1
-    print $ sum $ map (score y) x                         -- part 2
+        (x, y) = unzip valpairs
+        sortedX = sort x
+        sortedY = sort y
+    print $ sum $ map abs $ zipWith (-) sortedY sortedX -- part 1
+    print $ sum $ map (score y) x                       -- part 2
     
