4026dded7cc37513d9f2cc46952474b4c51faca4
Jonathan Carroll <github@jcarroll.com.au>
2025-12-03 22:00:06 +1030

    2025/03 R with improvement

---
+++ 2025/R/R/day03.R
@@ -0,0 +1,228 @@
+#' Day 03: Lobby
+#'
+#' [Lobby](https://adventofcode.com/2025/day/3)
+#'
+#' @name day03
+#' @rdname day03
+#' @details
+#'
+#' **Part One**
+#'
+#' You descend a short staircase, enter the surprisingly vast lobby, and
+#' are quickly cleared by the security checkpoint. When you get to the main
+#' elevators, however, you discover that each one has a red light above it:
+#' they\'re all *offline*.
+#'
+#' \"Sorry about that,\" an Elf apologizes as she tinkers with a nearby
+#' control panel. \"Some kind of electrical surge seems to have fried them.
+#' I\'ll try to get them online soon.\"
+#'
+#' You explain your need to get further underground. \"Well, you could at
+#' least take the escalator down to the printing department, not that
+#' you\'d get much further than that without the elevators working. That
+#' is, you could if the escalator weren\'t also
+#' [offline]{title="Escalator temporarily stairs. Sorry for the convenience."}.\"
+#'
+#' \"But, don\'t worry! It\'s not fried; it just needs power. Maybe you can
+#' get it running while I keep working on the elevators.\"
+#'
+#' There are batteries nearby that can supply emergency power to the
+#' escalator for just such an occasion. The batteries are each labeled with
+#' their [joltage](/2020/day/10) rating, a value from `1` to `9`. You make
+#' a note of their joltage ratings (your puzzle input). For example:
+#'
+#'     987654321111111
+#'     811111111111119
+#'     234234234234278
+#'     818181911112111
+#'
+#' The batteries are arranged into *banks*; each line of digits in your
+#' input corresponds to a single bank of batteries. Within each bank, you
+#' need to turn on *exactly two* batteries; the joltage that the bank
+#' produces is equal to the number formed by the digits on the batteries
+#' you\'ve turned on. For example, if you have a bank like `12345` and you
+#' turn on batteries `2` and `4`, the bank would produce `24` jolts. (You
+#' cannot rearrange batteries.)
+#'
+#' You\'ll need to find the largest possible joltage each bank can produce.
+#' In the above example:
+#'
+#' - In *`98`*`7654321111111`, you can make the largest joltage possible,
+#'   *`98`*, by turning on the first two batteries.
+#' - In *`8`*`1111111111111`*`9`*, you can make the largest joltage
+#'   possible by turning on the batteries labeled `8` and `9`, producing
+#'   *`89`* jolts.
+#' - In `2342342342342`*`78`*, you can make *`78`* by turning on the last
+#'   two batteries (marked `7` and `8`).
+#' - In `818181`*`9`*`1111`*`2`*`111`, the largest joltage you can produce
+#'   is *`92`*.
+#'
+#' The total output joltage is the sum of the maximum joltage from each
+#' bank, so in this example, the total output joltage is `98` + `89` +
+#' `78` + `92` = *`357`*.
+#'
+#' There are many batteries in front of you. Find the maximum joltage
+#' possible from each bank; *what is the total output joltage?*
+#'
+#' **Part Two**
+#'
+#' The escalator doesn\'t move. The Elf explains that it probably needs
+#' more joltage to overcome the [static
+#' friction](https://en.wikipedia.org/wiki/Static_friction){target="_blank"}
+#' of the system and hits the big red \"joltage limit safety override\"
+#' button. You lose count of the number of times she needs to confirm
+#' \"yes, I\'m sure\" and decorate the lobby a bit while you wait.
+#'
+#' Now, you need to make the largest joltage by turning on *exactly twelve*
+#' batteries within each bank.
+#'
+#' The joltage output for the bank is still the number formed by the digits
+#' of the batteries you\'ve turned on; the only difference is that now
+#' there will be *`12`* digits in each bank\'s joltage output instead of
+#' two.
+#'
+#' Consider again the example from before:
+#'
+#'     987654321111111
+#'     811111111111119
+#'     234234234234278
+#'     818181911112111
+#'
+#' Now, the joltages are much larger:
+#'
+#' - In *`987654321111`*`111`, the largest joltage can be found by turning
+#'   on everything except some `1`s at the end to produce *`987654321111`*.
+#' - In the digit sequence *`81111111111`*`111`*`9`*, the largest joltage
+#'   can be found by turning on everything except some `1`s, producing
+#'   *`811111111119`*.
+#' - In `23`*`4`*`2`*`34234234278`*, the largest joltage can be found by
+#'   turning on everything except a `2` battery, a `3` battery, and another
+#'   `2` battery near the start to produce *`434234234278`*.
+#' - In *`8`*`1`*`8`*`1`*`8`*`1`*`911112111`*, the joltage *`888911112111`*
+#'   is produced by turning on everything except some `1`s near the front.
+#'
+#' The total output joltage is now much larger: `987654321111` +
+#' `811111111119` + `434234234278` + `888911112111` = *`3121910778619`*.
+#'
+#' *What is the new total output joltage?*
+#'
+#' @param x some data
+#' @return For Part One, `f03a(x)` returns .... For Part Two,
+#'   `f03b(x)` returns ....
+#' @export
+#' @examples
+#' f03a(example_data_03())
+#' f03b()
+f03a <- function(x) {
+  # x <- readLines("input.txt")
+  # x <- readLines("inst/input03.txt")
+  sum(sapply(x, find_bat, 2))
+}
+
+# find_bat <- function(x) {
+#   x <- as.integer(strsplit(x, "")[[1]])
+#   best <- 0
+#   for (i in seq_along(x)) {
+#     for (j in seq_along(x)[-1]) {
+#       if (i >= j) next
+#       best <- max(best, 10*x[i] + x[j])
+#     }
+#   }
+#   best
+# }
+
+joltage <- function(digits, n) {
+  if (n == 0) return(NULL)
+  first <- head(digits, length(digits) - n + 1)
+  largest <- which.max(first)
+  paste0(digits[largest], joltage(digits[(largest+1):length(digits)], n - 1))
+}
+
+find_bat <- function(x, n) {
+  x <- as.integer(strsplit(x, "")[[1]])
+  as.numeric(joltage(x, n))
+}
+
+# first, brute-force pass
+# find_bat2 <- function(x) {
+#   x <- as.integer(strsplit(x, "")[[1]])
+#   best <- 0
+#   n <- length(x)
+#   for (ai in 1:(n-11)) {
+#     for (aj in (ai+1):(n-10)) {
+#       if (1e11*x[ai]+1e10*x[aj] < best) next
+#       for (ak in (aj+1):(n-9)) {
+#         if (1e11*x[ai]+1e10*x[aj]+1e9*x[ak] < best) next
+#         for (al in (ak+1):(n-8)) {
+#           if (1e11*x[ai]+1e10*x[aj]+1e9*x[ak]+1e8*x[al] < best) next
+#           for (am in (al+1):(n-7)) {
+#             if (1e11*x[ai]+1e10*x[aj]+1e9*x[ak]+1e8*x[al]+1e7*x[am] < best) next
+#             for (an in (am+1):(n-6)) {
+#               if (1e11*x[ai]+1e10*x[aj]+1e9*x[ak]+1e8*x[al]+1e7*x[am]+1e6*x[an] < best) next
+#               for (ao in (an+1):(n-5)) {
+#                 if (1e11*x[ai]+1e10*x[aj]+1e9*x[ak]+1e8*x[al]+1e7*x[am]+1e6*x[an]+1e5*x[ao] < best) next
+#                 for (ap in (ao+1):(n-4)) {
+#                   if (1e11*x[ai]+1e10*x[aj]+1e9*x[ak]+1e8*x[al]+1e7*x[am]+1e6*x[an]+1e5*x[ao]+1e4*x[ap] < best) next
+#                   for (aq in (ap+1):(n-3)) {
+#                     if (1e11*x[ai]+1e10*x[aj]+1e9*x[ak]+1e8*x[al]+1e7*x[am]+1e6*x[an]+1e5*x[ao]+1e4*x[ap]+1e3*x[aq] < best) next
+#                     for (ar in (aq+1):(n-2)) {
+#                       if (1e11*x[ai]+1e10*x[aj]+1e9*x[ak]+1e8*x[al]+1e7*x[am]+1e6*x[an]+1e5*x[ao]+1e4*x[ap]+1e3*x[aq]+1e2*x[ar] < best) next
+#                       for (as in (ar+1):(n-1)) {
+#                         if (1e11*x[ai]+1e10*x[aj]+1e9*x[ak]+1e8*x[al]+1e7*x[am]+1e6*x[an]+1e5*x[ao]+1e4*x[ap]+1e3*x[aq]+1e2*x[ar]+10*x[as] < best) next
+#                         for (at in (as+1):n) {
+#                           best <- max(best,
+#                                       1e11*x[ai]+
+#                                         1e10*x[aj]+
+#                                         1e9*x[ak]+
+#                                         1e8*x[al]+
+#                                         1e7*x[am]+
+#                                         1e6*x[an]+
+#                                         1e5*x[ao]+
+#                                         1e4*x[ap]+
+#                                         1e3*x[aq]+
+#                                         1e2*x[ar]+
+#                                         10*x[as]+
+#                                         x[at]
+#                           )
+#                         }
+#                       }
+#                     }
+#                   }
+#                 }
+#               }
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+#   best
+# }
+
+#' @rdname day03
+#' @export
+f03b <- function(x) {
+  # x <- readLines("input.txt")
+  # x <- readLines("inst/input03.txt")
+  sprintf("%16.0f", sum(sapply(x, find_bat, 12)))
+}
+
+
+f03_helper <- function(x) {
+
+}
+
+
+#' @param example Which example data to use (by position or name). Defaults to
+#'   1.
+#' @rdname day03
+#' @export
+example_data_03 <- function(example = 1) {
+  l <- list(
+    a = c(
+
+
+    )
+  )
+  l[[example]]
+}
